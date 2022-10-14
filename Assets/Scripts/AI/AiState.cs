4799c9c4aa76b30caa00ef9dadc4fb8af3ea8016
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-10-14 12:13:58 +1030

    Ai scripts

---
+++ Assets/Scripts/AI/AiState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiState
+{
+    public virtual void OnEnter()
+    {
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    public virtual void OnExit()
+    {
+
+    }
+}
