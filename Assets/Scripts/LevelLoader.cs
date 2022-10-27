d696db9f54300b3766fe40a55b1c1df4276cd99c
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-10-27 16:33:15 +1030

    Level transitions done-ish

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] CanvasGroup fadeObj;
+    [SerializeField] float transitionTime;
+    float transitionTimer = 0;
+    bool transitionOut = false,transitionIn = false;
+
+    public int level;
+
+    private void Start()
+    {
+        transitionIn = true;
+    }
+    private void Update()
+    {
+        if (transitionIn)
+        {
+            
+            if (fadeObj != null)
+            {
+                fadeObj.alpha =  1-(transitionTimer / transitionTime);
+            }
+            if (transitionTimer >= transitionTime)
+            {
+                transitionIn = false;
+                transitionTimer = 0;
+            }
+            transitionTimer += Time.deltaTime;
+        }
+        if (transitionOut)
+        {
+            
+            if(fadeObj != null)
+            {
+                fadeObj.alpha = transitionTimer / transitionTime;
+            }
+            if(transitionTimer >= transitionTime)
+            {
+                
+                LoadLvl();
+            }
+            transitionTimer += Time.deltaTime;
+        }
+    }
+
+    [ContextMenu("Run")]
+    public void Load()
+    {
+        transitionOut = true;
+    }
+    [ContextMenu("ForceLoad")]
+    void LoadLvl()
+    {
+        //todo check if the level is unlocked
+
+        if (SceneManager.sceneCountInBuildSettings > level + 1)
+            SceneManager.LoadScene(level + 1);
+    }
+}
+
+
+
