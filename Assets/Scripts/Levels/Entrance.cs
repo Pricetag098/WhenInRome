ebd6f4ecb1892c2ab808bf21130ab9ff1a239dab
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-11-03 14:36:02 +1030

    Added rooms

---
+++ Assets/Scripts/Levels/Entrance.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrance : MonoBehaviour
+{
+    public Room room;
+    private void OnTriggerEnter(Collider other)
+    {
+        room.Spawn();
+        enabled = false;
+    }
+}
--- Assets/Scripts/Levels/Room.cs
+++ Assets/Scripts/Levels/Room.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public List<GameObject> enemys = new List<GameObject>();
+    public bool complete;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(GameObject enemy in enemys)
+        {
+            enemy.SetActive(false);
+        }    
     }
 
+    public void Spawn()
+    {
+        foreach (GameObject enemy in enemys)
+        {
+            enemy.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (!complete)
+        {
+            bool allDead = true;
+            foreach (GameObject enemy in enemys)
+            {
+                if (enemy.activeSelf)
+                {
+                    allDead = false;
+                }
+            }
+            if (allDead)
+            {
+                complete = true;
+            }
+        }
     }
 }
