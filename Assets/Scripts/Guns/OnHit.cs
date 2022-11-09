a388c38266d3b4ae7dd0d39de0e4dff27ab4ec28
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-11-09 14:00:33 +1030

    Change to on hit

--- Assets/Scripts/Guns/OnHit.cs
+++ Assets/Scripts/Guns/OnHit.cs
@@ -5,13 +5,13 @@ using UnityEngine.Events;
 
 public class OnHit : MonoBehaviour
 {
-    public UnityEvent onHit;
-
+    //public UnityEvent onHit;
+    CombatMeter meter; 
     public float timeSinceLastHit = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meter = GetComponent<CombatMeter>();
     }
     
     // Update is called once per frame
@@ -19,9 +19,10 @@ public class OnHit : MonoBehaviour
     {
         timeSinceLastHit += Time.deltaTime;
     }
-    public void Hit()
+    public void Hit(HitData data)
     {
         timeSinceLastHit = 0;
-        onHit.Invoke();
+        if (meter != null)
+            meter.Charge(data);
     }
 }
