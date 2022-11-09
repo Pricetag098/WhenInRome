44b76818e09a211a5270a597a44520f6df62d186
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-11-10 09:10:18 +1030

    increades item pickup consistency

--- Assets/Scripts/Guns/WeaponPickup.cs
+++ Assets/Scripts/Guns/WeaponPickup.cs
@@ -12,6 +12,7 @@ public class WeaponPickup : MonoBehaviour
     public Holster holster;
     //float MaxPickUPDist = 5;
     //bool isholding = false;
+    bool insideCol = false;
     
     private void Start()
     {
@@ -26,30 +27,35 @@ public class WeaponPickup : MonoBehaviour
         newWeapon.GetComponent<ObjectPooler>().owner = holster.playerAim.gameObject;
         door.Open();
     }
-    public void OnTriggerStay(Collider coll)
-    {
-        
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            SpawnGun();
-            GetComponent<Collider>().enabled = false;
-            //GetComponent<MeshRenderer>().enabled = false;
-            transform.GetChild(0).gameObject.SetActive(false);
-            GetComponent<ObjectSpin>().enabled = false;
-            sound.Play();
-            holster.Equip();
-            enabled = false;
-            weaponHint.SetActive(false);
-        }
 
+    private void Update()
+    {
+        if (insideCol)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SpawnGun();
+                GetComponent<Collider>().enabled = false;
+                //GetComponent<MeshRenderer>().enabled = false;
+                transform.GetChild(0).gameObject.SetActive(false);
+                GetComponent<ObjectSpin>().enabled = false;
+                sound.Play();
+                holster.Equip();
+                enabled = false;
+                weaponHint.SetActive(false);
+            }
+        }
     }
+    
     public void OnTriggerEnter(Collider other)
     {
         EnbleHint();
+        insideCol = true;
     }
     public void OnTriggerExit(Collider other)
     {
         DisableHint();
+        insideCol = false;
     }
     public void EnbleHint()
     {
